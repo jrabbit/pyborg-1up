72ea1f6b285a3c25aa73db2f425030f2b1a63809
jrabbit <jackjrabbit@gmail.com>
2016-03-27 03:42:49 -0400

    ssl!

--- pyborg/pyborg-irc2.py
+++ pyborg/pyborg-irc2.py
@@ -1,17 +1,25 @@
+import ssl
+
+
 import baker
 import irc.bot
 import irc.strings
+import irc
 import toml
 
 import pyborg.pyborg
 
+
 class ModIRC(irc.bot.SingleServerIRCBot):
-    def __init__(self, my_pyborg, channel, nickname, server, port):
-        super(ModIRC, self).__init__([(server, port)], nickname, nickname)
+
+    def __init__(self, my_pyborg, channel, nickname, server, port, **connect_params):
+        super(ModIRC, self).__init__(
+            [(server, port)], nickname, nickname, **connect_params)
         self.my_pyborg = my_pyborg
         self.channel = channel
 
     def on_welcome(self, c, e):
+        # TODO Join channels from toml config
         c.join(self.channel)
 
     def on_pubmsg(self, c, e):
@@ -19,17 +27,18 @@ class ModIRC(irc.bot.SingleServerIRCBot):
         if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
             self.do_command(e, a[1].strip())
         return
+
     def do_command(self, e, cmd):
         nick = e.source.nick
         c = self.connection
         c.notice(nick, "Hello {}".format(nick))
 
 
-
 @baker.command(default=True)
 def start_irc_bot(channel, nickname, server, port=6697):
     pyb = pyborg.pyborg.pyborg()
-    bot = ModIRC(pyb, channel, nickname, server, port)
+    ssl_factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
+    bot = ModIRC(pyb, channel, nickname, server, port, connect_factory=ssl_factory)
     try:
         bot.start()
     except KeyboardInterrupt:
