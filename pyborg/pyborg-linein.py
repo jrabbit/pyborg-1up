9f7e0b2e8aae1a366845d7dd11646c611560a03d
jrabbit <jackjrabbit@gmail.com>
2016-05-01 21:06:48 -0400

    more multiplexing for #14

--- pyborg/pyborg-linein.py
+++ pyborg/pyborg-linein.py
@@ -9,12 +9,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#        
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -22,26 +22,34 @@
 import string
 import sys
 
+import baker
+import requests
+
 from pyborg import pyborg
 
+
 class ModLineIn(object):
+
     """
     Module to interface console input and output with the PyBorg learn
     and reply modules. Allows offline chat with PyBorg.
     """
     # Command list for this module
     commandlist = "LineIn Module Commands:\n!quit"
-    commanddict = { "quit": "Usage: !quit\nQuits pyborg and saves the dictionary" }
+    commanddict = {
+        "quit": "Usage: !quit\nQuits pyborg and saves the dictionary"}
 
-    def __init__(self, my_pyborg):
-        self.pyborg = my_pyborg
+    def __init__(self, my_pyborg, multiplexed):
+        self.multiplexed = multiplexed
+        if not multiplexed:
+            self.pyborg = my_pyborg()
         self.start()
 
     def start(self):
         print "PyBorg offline chat!\n"
         print "Type !quit to leave"
         print "What is your name"
-        name = raw_input("? ")
+        self.name = raw_input("? ")
         while 1:
             try:
                 body = raw_input("> ")
@@ -54,7 +62,13 @@ class ModLineIn(object):
                 if self.linein_commands(body):
                     continue
             # Pass message to borg
-            self.pyborg.process_msg(self, body, 100, 1, ( name ), owner = 1)
+            if self.multiplexed:
+                d = {"body": body, "reply_rate": 100, "learning": 1, "owner": 1}
+                resp = requests.post("http://localhost:2001/process", data=d)
+                resp.raise_for_status()
+                self.output(resp.text, None)
+            else:
+                self.pyborg.process_msg(self, body, 100, 1, (self.name), owner=1)
 
     def linein_commands(self, body):
         command_list = string.split(body)
@@ -67,16 +81,19 @@ class ModLineIn(object):
         """
         Output a line of text.
         """
-        message = message.replace("#nick", args)
+        message = message.replace("#nick", self.name)
         print message
 
-if __name__ == "__main__":
-    # start the pyborg
-    my_pyborg = pyborg.pyborg()
+
+@baker.command(default=True, shortopts={"multiplex": "m", })
+def start(multiplex=False):
+    my_pyborg = pyborg.pyborg
     try:
-        ModLineIn(my_pyborg)
+        ModLineIn(my_pyborg, multiplex)
     except SystemExit:
         pass
-    my_pyborg.save_all()
-    del my_pyborg
+    if not multiplex:
+        my_pyborg.save_all()
 
+if __name__ == "__main__":
+    baker.run()
--- pyborg/pyborg_http.py
+++ pyborg/pyborg_http.py
@@ -6,6 +6,12 @@ from pyborg.pyborg import pyborg
 our_pyborg = pyborg()
 
 
+@bottle.route("/")
+def index():
+    return "<h1>Welcome to Pyborg/http</h1>"
+
+# Basic API
+
 @bottle.route("/learn", method="POST")
 def learn():
     body = request.POST.get("body")
@@ -19,9 +25,27 @@ def reply():
     return our_pyborg.reply(body)
 
 
-@bottle.route("/")
-def index():
-    return "<h1>Welcome to Pyborg/http</h1>"
+# Advanced API
+
+class DumbyIOMod(object):
+    message = None
+    """fake IO mod for pyborg interop"""
+    def output(self, message, args):
+        self.message = message
+        self.args = args
+
+@bottle.route("/process", method="POST")
+def process():
+    body = request.POST.get("body")
+    reply_rate = request.POST.get("reply_rate")
+    learning = request.POST.get("learning")
+    owner = request.POST.get("owner")
+    io = DumbyIOMod()
+    our_pyborg.process_msg(io, body, reply_rate, learning, None, owner)
+    if io.message:
+        return io.message
+    else:
+        return ""
 
 if __name__ == '__main__':
     bottle.run(host="localhost", port=2001, reloader=True)
