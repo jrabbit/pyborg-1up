5b95cebf7a7f662af0fa9522bb1f6557050a0839
jrabbit <jackjrabbit@gmail.com>
2016-05-08 14:36:41 -0400

    fixes tests they pass in multiplex mode

--- pyborg/test_irc2.py
+++ pyborg/test_irc2.py
@@ -6,28 +6,31 @@ import irc.client
 import pyborg_irc2
 import pyborg
 
-class TestConnect(unittest.TestCase):
-    @mock.patch('irc.connection.socket')
-    # @mock.patch('socket.socket')
-    def Disabled_test_connect(self, patched_wrap, ):
-        # This test is broken.
-        mod = pyborg_irc2.ModIRC(pyborg.pyborg.pyborg())
-        mod.start()
-        # print patched_wrap.call_args
-        mod.disconnect()
+class TestReplys(unittest.TestCase):
+    # @mock.patch('socket.socket.connect')
+    # # @mock.patch('socket.socket')
+    # def test_connect(self, patched_wrap, ):
+    #     # This test is doesn't test anything and starts a loop
+    #     mod = pyborg_irc2.ModIRC(pyborg.pyborg.pyborg)
+    #     mod.start()
+    #     print patched_wrap.call_args
+    #     mod.disconnect()
     
-    @mock.patch('pyborg.pyborg.pyborg.learn')
+    @mock.patch('pyborg_irc2.ModIRC.strip_nicks', side_effect=lambda x, _: x)
+    @mock.patch('pyborg_irc2.ModIRC.learn')
     @mock.patch('irc.connection')
-    def test_respond(self, c, learn):
-        mod = pyborg_irc2.ModIRC(pyborg.pyborg.pyborg())
-        our_event = irc.client.Event(type=None, source=None, target=None, arguments=['*', '*', 'nick'])
+    def test_respond(self, c, learn, stripnicks):
+        mod = pyborg_irc2.ModIRC(pyborg.pyborg.pyborg)
+        our_event = irc.client.Event(type=None, source=None, target=u"#ranarchism", arguments=[u"Hello Pyborg"])
         mod.on_pubmsg(c, our_event)
         learn.assert_called_with(our_event.arguments[0].encode('utf-8'))
-    @mock.patch('pyborg.pyborg.pyborg.reply')
-    @mock.patch('pyborg.pyborg.pyborg.learn')
+    
+    @mock.patch('pyborg_irc2.ModIRC.strip_nicks', side_effect=lambda x, _: x)
+    @mock.patch('pyborg_irc2.ModIRC.reply')
+    @mock.patch('pyborg_irc2.ModIRC.learn')
     @mock.patch('irc.connection')
-    def test_nick_reply(self, c, learn, reply):
-        mod = pyborg_irc2.ModIRC(pyborg.pyborg.pyborg())
+    def test_nick_reply(self, c, learn, reply, stripnicks):
+        mod = pyborg_irc2.ModIRC(pyborg.pyborg.pyborg)
         # nick = mod.connection.get_nickname()
         nick = 'steve'
         # c.get_nickname.return_value = "steve"
