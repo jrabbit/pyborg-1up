0f68ecd3977961b1891b7f971f4cbd437be6e9e8
Jack Laxson <jackjrabbit@gmail.com>
2016-09-07 19:20:46 -0400

    discord module beginning

---
+++ pyborg/pyborg_discord.py
@@ -0,0 +1,90 @@
+import sys
+import logging
+
+import arrow
+import requests
+import baker
+import toml
+
+import discord
+import asyncio
+
+import pyborg.pyborg
+
+#https://github.com/Rapptz/discord.py/blob/master/discord/ext/commands/bot.py#L146
+
+class PyborgDiscord(discord.Client):
+    """docstring for PyborgDiscord"""
+    def __init__(self, toml_file):
+        super(PyborgDiscord, self).__init__()
+        self.toml_file = toml_file
+        self.settings = toml.load(self.toml_file)
+        self.multiplexing = self.settings['pyborg']['multiplex']
+        if not self.multiplexing:
+            # self.pyborg = pyborg.pyborg.pyborg()
+            # pyb config parsing isn't ready for python 3.
+            raise NotImplementedError
+        else:
+            self.pyborg = None
+
+    def our_start(self):
+        self.run(self.settings['discord']['token'])
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        print(message.content)
+        if self.settings['discord']['learning']:
+            self.learn(message.content)
+        if message.content.startswith("<@{}>".format(self.user.id)):
+            msg = self.reply(message.content)
+            print(" on message: %s" % msg)
+            if msg:
+                print("Sending message...")
+                await self.send_message(message.channel, msg)
+
+    def learn(self, body):
+        """thin wrapper for learn to switch to multiplex mode"""
+        if self.settings['pyborg']['multiplex']:
+            ret = requests.post("http://localhost:2001/learn", data={"body": body})
+            if ret.status_code > 499:
+                logger.error("Internal Server Error in pyborg_http. see logs.")
+            else:
+                ret.raise_for_status()
+
+    def reply(self, body):
+        "thin wrapper for reply to switch to multiplex mode"
+        if self.settings['pyborg']['multiplex']:
+            ret = requests.post("http://localhost:2001/reply", data={"body": body})
+            if ret.status_code == requests.codes.ok:
+                reply = ret.text
+                print("got reply")
+                print(reply)
+            elif ret.status_code > 499:
+                logger.error("Internal Server Error in pyborg_http. see logs.")
+                return
+            else:
+                ret.raise_for_status()
+            return reply
+
+
+@baker.command(default=True, shortopts={"toml_conf": "f"})
+def start_discord_bot(verbose=True, toml_conf="example.discord.toml"):
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+    bot = PyborgDiscord(toml_conf)
+    try:
+        bot.our_start()
+    except KeyboardInterrupt:
+        bot.teardown()
+        sys.exit()
+    except Exception:
+        bot.teardown()
+        raise
+
+if __name__ == '__main__':
+    baker.run()
