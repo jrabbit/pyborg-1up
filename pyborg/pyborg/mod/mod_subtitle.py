12b9a43abb23e84cc789109df7257f105d251492
Jack Laxson <jackjrabbit@gmail.com>
2017-08-30 20:08:23 -0400

    version bump, working subtitle module

--- pyborg/pyborg/mod/mod_subtitle.py
+++ pyborg/pyborg/mod/mod_subtitle.py
@@ -1,9 +1,11 @@
+import asyncio
 import datetime
 import logging
 import time
 
 import aeidon  # https://github.com/otsaloma/gaupol/tree/master/aeidon
 import attr
+import requests
 import toml
 
 logger = logging.getLogger(__name__)
@@ -12,6 +14,9 @@ logger = logging.getLogger(__name__)
 class PyborgSubtitles(object):
     conf_file = attr.ib()
     subs_file = attr.ib()
+    paused = attr.ib(default=False)
+    riffs = attr.ib(default=dict())
+    pre_processed = attr.ib(default=False)
 
     # def __attrs_post_init__(self):
     #    pass
@@ -28,20 +33,49 @@ class PyborgSubtitles(object):
         self.subtitles = self.project.subtitles
         self.run()
 
+    def clean(self, subtitle):
+        text = subtitle.replace("{\i1}", "").replace("{\i0}", "")
+        return text
+
     def run(self):
         """As time goes on print out our riffs"""
-        for subtitle in self.subtitles:
+        for idx, subtitle in enumerate(self.subtitles):
             # remove italics markup
-            text = subtitle.main_text.replace("{\i1}", "").replace("{\i0}", "")
-        
+            text = self.clean(subtitle.main_text)
+
             if text.startswith("("):
                 # Should we respond to sound effects?
-                pass
+                print(text)
             else:
                 print(text)
+                if self.pre_processed:
+                    print(">", self.riffs[idx])
+                else:
+                    print(">", self.reply(text))
             time.sleep(subtitle.duration_seconds)
         
         self.teardown()
+
+    def pre_process(self):
+        for i, sub in enumerate(self.subtitles):
+            reply = self.reply(self.clean(sub.main_text))
+            if reply:
+                self.riffs[i] = reply
+
+
+    def reply(self, body):
+        """thin wrapper for reply to switch to multiplex mode"""
+        if self.multiplexing:
+            ret = requests.post("http://{}:2001/reply".format(self.multi_server), data={"body": body})
+            if ret.status_code == requests.codes.ok:
+                reply = ret.text
+                logger.debug("got reply: %s", reply)
+            elif ret.status_code > 499:
+                logger.error("Error: Internal Server Error in pyborg_http. see logs.")
+                return
+            else:
+                ret.raise_for_status()
+            return reply
 
     def teardown(self):
         pass
