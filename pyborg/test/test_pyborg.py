a43425473146f7f5e2d60e1e412b0c21d6d03fca
Jack Laxson <jackjrabbit@gmail.com>
2017-09-08 03:17:18 -0400

    pep8

--- pyborg/test/test_pyborg.py
+++ pyborg/test/test_pyborg.py
@@ -106,38 +106,39 @@ class TestPyborgSave(unittest.TestCase):
     blank_brain_path = "pyborg/test/fixtures/blank.brain.pyborg.json"
 
     lines = {713833202: ['destroy edgar', 1],
-                      1071494628: ['murder edgar', 1],
-                      2622503271: ['kill edgar', 2],
-                      3025701897: ['fuck the moon', 1],
-                      3127776909: ['fuck space', 1],
-                      3710277035: ['are you a murderer', 1],
-                      3953240527: ['fuck reddit', 1],
-                      4186136012: ['fuck', 1]}
+             1071494628: ['murder edgar', 1],
+             2622503271: ['kill edgar', 2],
+             3025701897: ['fuck the moon', 1],
+             3127776909: ['fuck space', 1],
+             3710277035: ['are you a murderer', 1],
+             3953240527: ['fuck reddit', 1],
+             4186136012: ['fuck', 1]}
 
     words = {'a': [{'hashval': 3710277035, 'index': 2}],
-                      'are': [{'hashval': 3710277035, 'index': 0}],
-                      'destroy': [{'hashval': 713833202, 'index': 0}],
-                      'edgar': [{'hashval': 2622503271, 'index': 1},
-                                {'hashval': 713833202, 'index': 1},
-                                {'hashval': 1071494628, 'index': 1}],
-                      'fuck': [{'hashval': 4186136012, 'index': 0},
-                               {'hashval': 3025701897, 'index': 0},
-                               {'hashval': 3127776909, 'index': 0},
-                               {'hashval': 3953240527, 'index': 0}],
-                      'kill': [{'hashval': 2622503271, 'index': 0}],
-                      'moon': [{'hashval': 3025701897, 'index': 2}],
-                      'murder': [{'hashval': 1071494628, 'index': 0}],
-                      'murderer': [{'hashval': 3710277035, 'index': 3}],
-                      'reddit': [{'hashval': 3953240527, 'index': 1}],
-                      'space': [{'hashval': 3127776909, 'index': 1}],
-                      'the': [{'hashval': 3025701897, 'index': 1}],
-                      'you': [{'hashval': 3710277035, 'index': 1}]}
+             'are': [{'hashval': 3710277035, 'index': 0}],
+             'destroy': [{'hashval': 713833202, 'index': 0}],
+             'edgar': [{'hashval': 2622503271, 'index': 1},
+                       {'hashval': 713833202, 'index': 1},
+                       {'hashval': 1071494628, 'index': 1}],
+             'fuck': [{'hashval': 4186136012, 'index': 0},
+                      {'hashval': 3025701897, 'index': 0},
+                      {'hashval': 3127776909, 'index': 0},
+                      {'hashval': 3953240527, 'index': 0}],
+             'kill': [{'hashval': 2622503271, 'index': 0}],
+             'moon': [{'hashval': 3025701897, 'index': 2}],
+             'murder': [{'hashval': 1071494628, 'index': 0}],
+             'murderer': [{'hashval': 3710277035, 'index': 3}],
+             'reddit': [{'hashval': 3953240527, 'index': 1}],
+             'space': [{'hashval': 3127776909, 'index': 1}],
+             'the': [{'hashval': 3025701897, 'index': 1}],
+             'you': [{'hashval': 3710277035, 'index': 1}]}
 
     @mock.patch("json.loads")
     @mock.patch("toml.load")
     def test_save_db(self, patched_toml, patched_json):
         patched_toml.return_value = {"pyborg-core": {"max_words": False}}
-        patched_json.return_value = {'version': u"1.4.0", 'words': self.words, 'lines':self.lines}
+        patched_json.return_value = {
+            'version': u"1.4.0", 'words': self.words, 'lines': self.lines}
         # m = mock.mock_open()
         # with mock.patch('pyborg.pyborg.open', m):
         our_pyb = pyborg.pyborg.pyborg(brain="/bogus/path")
@@ -145,7 +146,6 @@ class TestPyborgSave(unittest.TestCase):
         our_pyb.save_brain()
 
         # print(m.mock_calls)
-
 
 
 class TestPyborgLearning(unittest.TestCase):
