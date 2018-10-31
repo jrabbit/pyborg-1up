0edeccabfd948f9486005b9a14cf7082d50698e9
Jack Laxson <jackjrabbit@gmail.com>
2018-10-31 19:20:24 -0400

    blacken

--- pyborg/pyborg/cfgfile.py
+++ pyborg/pyborg/cfgfile.py
@@ -9,17 +9,18 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#        
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 import string
+
 
 def _load_config(filename):
     """
@@ -36,22 +37,22 @@ def _load_config(filename):
     while 1:
         line = line + 1
         s = f.readline()
-        if s=="":
+        if s == "":
             break
-        if s[0]=="#":
+        if s[0] == "#":
             continue
-            
-        #read if the string is above multiple lines
+
+        # read if the string is above multiple lines
         while s.find("#") == -1:
             lecture = f.readline()
             if lecture == "":
                 break
 
-            #Convert old configuration system ( with \ at the end of line )
-            if s[-2] == '\\':
+            # Convert old configuration system ( with \ at the end of line )
+            if s[-2] == "\\":
                 s = s[:-2]
 
-            s = s[:s.rfind("\n")] + lecture
+            s = s[: s.rfind("\n")] + lecture
             line = line + 1
 
         s = string.split(s, "=")
@@ -59,10 +60,11 @@ def _load_config(filename):
             stuff[string.strip(s[0])] = eval(string.strip(string.join(s[1:], "=")))
         except:
             print("Malformed line in %s line %d" % (filename, line))
-            print("\t%s" %s)
+            print("\t%s" % s)
             continue
     return stuff
-        
+
+
 def _save_config(filename, fields):
     """
     fields should be a dictionary. Keys as names of
@@ -72,23 +74,23 @@ def _save_config(filename, fields):
 
     # write the values with comments. this is a silly comment
     for key in fields.keys():
-        f.write("# "+fields[key][0]+" #\n")
+        f.write("# " + fields[key][0] + " #\n")
         s = repr(fields[key][1])
-        f.write(key+"\t= ")
+        f.write(key + "\t= ")
 
-        #Create a new line after each dic entry
+        # Create a new line after each dic entry
         if s.find("],") != -1:
             cut_string = ""
             while s.find("],") != -1:
-                position = s.find("],")+3
-                #cut_string = cut_string + s[:position] + "\\\n\t"
+                position = s.find("],") + 3
+                # cut_string = cut_string + s[:position] + "\\\n\t"
                 cut_string = cut_string + s[:position] + "\n\t"
                 s = s[position:]
             s = cut_string + s
-            f.write(s+"\n")
+            f.write(s + "\n")
             continue
 
-        #If the line exceed a normal display ( 80 col ) cut it
+        # If the line exceed a normal display ( 80 col ) cut it
         # if len(s) > 80:
         #   cut_string = ""
         #   while len(s) > 80:
@@ -97,9 +99,9 @@ def _save_config(filename, fields):
         #       cut_string = cut_string + s[:position] + "\n\t\t"
         #       s = s[position:]
         #   s = cut_string + s
-        f.write(s+"\n")
+        f.write(s + "\n")
 
-    #f.write("# End of configuration #")
+    # f.write("# End of configuration #")
     f.close()
 
 
@@ -140,4 +142,3 @@ class cfgset:
             keys[i] = (comment, self.__dict__[i])
         # save to config file
         _save_config(self._filename, keys)
-
