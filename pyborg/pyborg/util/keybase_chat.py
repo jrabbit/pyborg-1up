184d35a7e9a86726fd8f2b7ea8805a9165622ad9
Jack Laxson <jackjrabbit@gmail.com>
2019-01-12 06:15:33 -0500

    begin work on keybase

---
+++ pyborg/pyborg/util/keybase_chat.py
@@ -0,0 +1,13 @@
+import sys
+import json
+import subprocess
+
+def main(args):
+    "keybase chat util for pyborg testing entrypoint"
+    command = ["keybase", "chat", "api", "-m", json.dumps({"method": "list"})]
+    x = subprocess.run(command)
+    print(x)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
