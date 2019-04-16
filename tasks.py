b2d23240d6879cd96656c6bbc7190c62dd76c267
Jack Laxson <jackjrabbit@gmail.com>
2019-04-16 12:43:13 -0400

    docs & style fixes

--- tasks.py
+++ tasks.py
@@ -1,8 +1,11 @@
 from invoke import task
 from fabric2 import Connection
 
+
 @task
 def deploy(c, restart=False, sync=False):
+    "push code to a server (configure your own here)"
+
     c.run("git push --all")
     conn = Connection("trotsky")
     with conn.cd("src/pyborg-1up"):
@@ -11,36 +14,49 @@ def deploy(c, restart=False, sync=False):
         conn.run("git pull")
         conn.run("git stash pop")
         if sync:
-            conn.run("~/.local/bin/pipenv sync") # they all use the same pipenv managed virtualenv
+            conn.run("~/.local/bin/pipenv sync")  # they all use the same pipenv managed virtualenv
         if restart:
             for unit in ["pyborg_discord", "pyborg_http", "pyborg_twitter", "pyborg_mastodon"]:
                 conn.sudo("systemctl restart {}".format(unit), pty=True)
         print("Deploy Completed.")
 
+
 @task
-def release(c, clean=True):
+def release(c, clean=True, docker=False):
     "cut a release of pyborg"
+
     with c.cd("pyborg"):
         if clean:
             c.run("rm -rf build")
         c.run("pipenv run python --version", echo=True)
         c.run("pipenv run python setup.py bdist_wheel sdist")
         print("now run `gpg -ba` on the files in dist/ and upload with `twine`")
+    if docker:
+        # Build and push jrabbit/pyborg[:3], needs working d-c and docker
+        c.run("docker-compose build")
+        c.run("docker-compose push")
+
 
 @task
 def bandit(c):
+    "security audit tool"
     c.run("pipenv run bandit --exclude=build,test -s B311 -r pyborg", pty=True)
+
 
 @task
 def test(c):
     c.run("tox")
 
+
 @task
 def outdated(c):
+    "outdated packages"
     c.run("pipenv run pip list -o --format=columns")
+
 
 @task
 def lint(c, mypy=True):
+    "style & type checks"
     if mypy:
         c.run("pipenv run mypy pyborg/pyborg", warn=True)
     c.run("flake8 --config=tox.ini --count pyborg", warn=True)
