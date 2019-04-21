b28497bfa1c15cc6699d4aa862953b5a13fded26
Jack Laxson <jackjrabbit@gmail.com>
2019-04-21 10:42:28 -0400

    add asynctest helper to make fancier magicmocks.

--- pyborg/test/test_discord.py
+++ pyborg/test/test_discord.py
@@ -2,11 +2,13 @@ import logging
 import sys
 import unittest
 
-if sys.version_info >= (3,) and sys.version_info <= (3, 7):
+if sys.version_info >= (3,):
     from unittest import mock
     from functools import partial
     import asyncio
 
+    import asynctest
+    
     import pyborg
     from pyborg.util.utils_testing import do_nothing
 
@@ -16,7 +18,7 @@ if sys.version_info >= (3,) and sys.version_info <= (3, 7):
         def setUp(self):
             self.loop = asyncio.new_event_loop()
             asyncio.set_event_loop(self.loop)
-            self.loop.set_debug(True)
+            #self.loop.set_debug(True)
 
         def tearDown(self):
             self.loop.close()
@@ -30,7 +32,6 @@ if sys.version_info >= (3,) and sys.version_info <= (3, 7):
             patched_reply.return_value = ""
             patched_user.mentioned_in.return_value = False
             our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
-            our_pybd.send_typing = partial(do_nothing, "bogus_arg")
 
             self.loop.run_until_complete(our_pybd.on_message(msg))
             patched_reply.assert_not_called()
@@ -40,25 +41,18 @@ if sys.version_info >= (3,) and sys.version_info <= (3, 7):
         @mock.patch("pyborg.mod.mod_discord.PyborgDiscord.learn")
         @mock.patch("pyborg.mod.mod_discord.PyborgDiscord.reply")
         def test_reply(self, patched_reply, patched_learn, patched_user, patched_normalize):
-            msg = mock.MagicMock()
+            msg = asynctest.MagicMock()
             msg.content.return_value = "<@221134985560588289> you should play dota!"
             msg.content.split.return_value = ["<@!221134985560588289>", "you", "should", "play", "dota!"]
             msg.channel.return_value = "maketotaldestroy"
             msg.author.mention.return_value = "<@42303631157544375>"
-
+            msg.channel.send = asynctest.CoroutineMock()
             patched_user.return_value.id = "221134985560588289"
             patched_reply.return_value = "I should play dota!"
             patched_reply.replace.return_value = "I should play dota!"
 
             our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
-            our_pybd.send_message = do_nothing
-            our_pybd.send_typing = partial(do_nothing, "bogus_arg")
-
             self.loop.run_until_complete(our_pybd.on_message(msg))
-            # print(our_pybd.user.id)
-            # patched_user.id.assert_called_once_with()
-            # print(patched_reply.mock_calls, patched_learn.mock_calls)
-            # print(patched_send.mock_calls)
             patched_learn.assert_called_once_with(patched_normalize.return_value)
             patched_reply.assert_called_once_with(patched_normalize.return_value)
 
@@ -67,10 +61,11 @@ if sys.version_info >= (3,) and sys.version_info <= (3, 7):
         @mock.patch("pyborg.mod.mod_discord.PyborgDiscord.learn")
         @mock.patch("pyborg.mod.mod_discord.PyborgDiscord.reply")
         def test_nick_replace(self, patched_reply, patched_learn, patched_user, patched_normalize):
-            msg = mock.MagicMock()
+            msg = asynctest.MagicMock()
             msg.content.return_value = "<@221134985560588289> you should play dota!"
             msg.content.split.return_value = ["<@!221134985560588289>", "you", "should", "play", "dota!"]
             msg.channel.return_value = "maketotaldestroy"
+            msg.channel.send = asynctest.CoroutineMock()
             msg.author.mention.return_value = "<@42303631157544375>"
 
             patched_user.return_value.id = "221134985560588289"
