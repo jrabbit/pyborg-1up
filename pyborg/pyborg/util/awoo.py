1e69c261592eea20f52ebf0efb221dbda9774850
Jack Laxson <jackjrabbit@gmail.com>
2019-04-23 06:02:13 -0400

    we can simplify since we don't act on the match. thx mypy

--- pyborg/pyborg/util/awoo.py
+++ pyborg/pyborg/util/awoo.py
@@ -7,12 +7,6 @@ logger = logging.getLogger(__name__)
 def normalize_awoos(inp: str) -> str:
     "Takes an awoo of varying length and trims it."
     # https://docs.python.org/2/library/re.html#text-munging
-
-    def repl(m):
-        m.group()
-        return "awoo"
-
     pattern = re.compile(r"(?i)awo+")
-    # out = re.sub(pattern, repl, inp)
-    out = pattern.sub(repl, inp)
+    out = pattern.sub("awoo", inp)
     return out
