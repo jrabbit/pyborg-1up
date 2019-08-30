8474cbb6ae7db81d65daa8e307fccbb9a403e096
Jack Laxson <jackjrabbit@gmail.com>
2019-08-29 23:28:50 -0400

    try async test

--- pyborg/test/test_discord.py
+++ pyborg/test/test_discord.py
@@ -14,14 +14,15 @@ if sys.version_info >= (3,):
 
     logging.basicConfig(level=logging.DEBUG)
 
-    class TestOnMessage(unittest.TestCase):
+    class TestOnMessage(asynctest.TestCase):
         def setUp(self):
             self.loop = asyncio.new_event_loop()
             asyncio.set_event_loop(self.loop)
             # self.loop.set_debug(True)
 
-        def tearDown(self):
+        async def tearDown(self):
             self.loop.close()
+            await self.our_pybd.teardown()
 
         @mock.patch("pyborg.mod.mod_discord.PyborgDiscord.user", create=True)
         @asynctest.mock.patch("pyborg.mod.mod_discord.PyborgDiscord.learn")
@@ -31,9 +32,9 @@ if sys.version_info >= (3,):
             msg.content = "Yolo!"
             patched_reply.return_value = ""
             patched_user.mentioned_in.return_value = False
-            our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
+            self.our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
 
-            self.loop.run_until_complete(our_pybd.on_message(msg))
+            self.loop.run_until_complete(self.our_pybd.on_message(msg))
             patched_reply.assert_not_called()
 
         @mock.patch("pyborg.mod.mod_discord.normalize_awoos")
@@ -51,8 +52,8 @@ if sys.version_info >= (3,):
             patched_reply.return_value = "I should play dota!"
             patched_reply.replace.return_value = "I should play dota!"
 
-            our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
-            self.loop.run_until_complete(our_pybd.on_message(msg))
+            self.our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
+            self.loop.run_until_complete(self.our_pybd.on_message(msg))
             patched_learn.assert_called_once_with(patched_normalize.return_value)
             patched_reply.assert_called_once_with(patched_normalize.return_value)
 
@@ -72,11 +73,11 @@ if sys.version_info >= (3,):
             patched_reply.return_value = "I should play dota! #nick"
             patched_reply.replace.return_value = "I should play dota! <@42303631157544375>"
 
-            our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
-            our_pybd.send_message = do_nothing
-            our_pybd.send_typing = partial(do_nothing, "bogus_arg")
+            self.our_pybd = pyborg.mod.mod_discord.PyborgDiscord("pyborg/test/fixtures/discord.toml")
+            self.our_pybd.send_message = do_nothing
+            self.our_pybd.send_typing = partial(do_nothing, "bogus_arg")
 
-            self.loop.run_until_complete(our_pybd.on_message(msg))
+            self.loop.run_until_complete(self.our_pybd.on_message(msg))
             # print(asyncio.Task.all_tasks())
             patched_learn.assert_called_once_with(patched_normalize.return_value)
             patched_reply.assert_called_once_with(patched_normalize.return_value)
