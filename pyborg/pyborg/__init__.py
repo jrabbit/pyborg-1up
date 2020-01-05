05c7c72d864320beccf85768ed34f34bc3651dab
Jack Laxson <jackjrabbit@gmail.com>
2020-01-05 00:49:53 -0500

    fancy new version number methods

--- pyborg/pyborg/__init__.py
+++ pyborg/pyborg/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "2.0a3"
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import version as get_version
+else:
+    from importlib_metadata import version as get_version
+
+__version__ = get_version("pyborg")
