54656fc02bc5d70a69ee4aef8855da382863b02f
Jack Laxson <jackjrabbit@gmail.com>
2020-03-10 17:59:38 -0400

    current status

--- dbexpr/db_tester.py
+++ dbexpr/db_tester.py
@@ -1,46 +1,49 @@
-import asyncio
-import aiopg
-from aiopg.sa import create_engine
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Word(Base):
-    __tablename__ = 'word'
+    __tablename__ = "word"
     id = Column(Integer, primary_key=True)
     word = Column(String)
-    entries = relationship("Entry", back_populates="word")
+#    entries = relationship("Entry", back_populates="word")
+
+    def __repr__(self):
+        return f"word> {self.word} id> {self.id}"
+
 
 class Entry(Base):
-    __tablename__ = 'entry_thru'
+    __tablename__ = "entry_thru"
     id = Column(Integer, primary_key=True)
     index = Column(Integer)
-    line_id = Column(Integer, ForeignKey('line.id'))
+    line_id = Column(Integer, ForeignKey("line.id"))
     line = relationship("Line")
+
+    def __repr__(self):
+        return f"entry> id>{self.id}"
+
+
 class Line(Base):
-    __tablename__ = 'line'
+    __tablename__ = "line"
     id = Column(Integer, primary_key=True)
     entry = Column(String)
     magic = Column(Integer)
     # there's extra data in the db a integer
 
+
 class PybMeta(Base):
-    __tablename__ = 'settings'
+    __tablename__ = "settings"
     version = Column(String, primary_key=True)
 
 
-async def go():
-    async with create_engine(user="pyborg", database="pyborg", host="db", password="0V1!v@aS") as engine:
-        async with engine.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute("SELECT 1")
-                ret = []
-                async for row in cur:
-                    ret.append(row)
-                assert ret == [(1,)]
-    print("ALL DONE")
+if __name__ == "__main__":
+    engine = create_engine("postgresql://pyborg:trustno1@db/pyborg")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(go())
+    sess = Session()
+    sess.add(PybMeta(version="__EXPERIMENTAL__"))
+    sess.commit()
