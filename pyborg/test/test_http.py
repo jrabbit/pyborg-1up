c4d760a59b66b4be194b511a3a5ee7e13020a784
Jack Laxson <jackjrabbit@gmail.com>
2020-12-04 03:05:30 -0500

    make travis tests pass, dry bit me in the ass

--- pyborg/test/test_http.py
+++ pyborg/test/test_http.py
@@ -23,9 +23,10 @@ class TestIntegratesFullServerReply(unittest.TestCase):
     def setUp(self):
         self.tmp_f, self.tmp_path = tempfile.mkstemp()
         # start http server directly
-        bottle.install(BottledPyborg(brain_path=self.tmp_path))
-        app = bottle.default_app()
-        self.app = TestApp(app)
+        with mock.patch("toml.load", return_value={"pyborg-core": {"max_words": False}}) as _:
+            bottle.install(BottledPyborg(brain_path=self.tmp_path))
+            app = bottle.default_app()
+            self.app = TestApp(app)
 
     def test_learns(self, patched_close, patched_brain, patched_toml, _, __):
         ret = self.app.post("/learn", {"body": "pee pee"})
